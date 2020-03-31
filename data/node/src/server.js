43bad2afd8a995d5501107cfb5a307f439d3f51c
GiulianoWF <Giuliano.Floriani@edu.pucrs.br>
2020-03-31 17:29:49 -0300

    Sending POST message to ros2 topic.

--- data/node/src/server.js
+++ data/node/src/server.js
@@ -1,30 +1,34 @@
-const express = require('express');
-const rclnodejs = require('rclnodejs');
-const app = express();
-const routes = require('./routes.js')
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const rclnodejs     = require('rclnodejs');
+
 
 rclnodejs.init()
     .then(() => {
         var node = rclnodejs.createNode('publisher_example_node');
 
-        const publisher = node.createPublisher('std_msgs/msg/String', 'topic');
-        publisher.publish(`Hello ROS 2.0 from rclnodejs`);
+        const publisher = node.createPublisher('std_msgs/msg/String', 'send_message');
 
         node.createSubscription('std_msgs/msg/String', 'chatter', msg => {
             console.log(`Received message: ${typeof msg}`, msg);
             topic_message = msg;
         });
 
-        rclnodejs.spin(node);
+        var app = express();
+        app.use(bodyParser.urlencoded({extended: false}));
+        app.use(bodyParser.json());
 
+        app.get('/', function (req, res) {
+        res.send('Hello World!');
+        });
+
+        app.post('/', function(req,res) {
+            console.log(req.body.message);
+            publisher.publish(req.body.message);
+            res.json(req.body.message);
+        });
+        rclnodejs.spin(node);
         console.log('ros2 spin');
+        app.listen(3000);
     });
 
-app.get('/', function(req, res) {
-    res.json(topic_message);
-});
-
-
-//app.use(routes);
-app.use(express.json);
-app.listen(3000);
