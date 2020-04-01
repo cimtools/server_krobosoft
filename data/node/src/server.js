f679e95a96fe42ad5e6d00bc654dc2dc96a86463
GiulianoWF <Giuliano.Floriani@edu.pucrs.br>
2020-04-01 09:05:40 -0300

    Change app scope. Change routes. Serving index.html containing React on '/'.

--- data/node/src/server.js
+++ data/node/src/server.js
@@ -2,10 +2,18 @@ const express       = require('express');
 const bodyParser    = require('body-parser');
 const rclnodejs     = require('rclnodejs');
 
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen(3000);
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname+'/index.html');
+});
 
 rclnodejs.init()
     .then(() => {
-        var node = rclnodejs.createNode('publisher_example_node');
+        const node = rclnodejs.createNode('publisher_example_node');
 
         const rospublisher = node.createPublisher('std_msgs/msg/String', 'send_message');
         const subscription = node.createSubscription('std_msgs/msg/String', 'chatter', msg => {
@@ -13,23 +21,17 @@ rclnodejs.init()
             topic_message = msg;
         });
 
-        var app = express();
-        app.use(bodyParser.urlencoded({extended: false}));
-        app.use(bodyParser.json());
-
-        app.get('/', function (req, res) {
+        app.get('/last_message', function(req, res){
             res.send(topic_message);
         });
 
-        app.post('/', function(req,res) {
+        app.post('/send_command', function(req,res) {
             console.log(req.body.message);
             rospublisher.publish(req.body.message);
             res.json(req.body.message);
         });
 
-        app.listen(3000);
-
         rclnodejs.spin(node);
         console.log('ros2 spin');
-    });
+});
 
