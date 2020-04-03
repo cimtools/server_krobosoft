5e1d6c27a3e58ac06450467d43b20de72bdf1e52
Gustavo Leal <gustavohleal96@gmail.com>
2020-04-02 22:17:10 -0300

    Multiples subscription per connection issue solved

--- data/node/public/js/terminal.js
+++ data/node/public/js/terminal.js
@@ -1,13 +1,12 @@
 const socket = io();
 
 socket.on('message', (socket) => {
-    //console.log(socket);
+    console.log('Receiv: ', socket);
 })
 
 document.querySelector('#terminal').addEventListener('submit', (e) => {
     e.preventDefault();
     const command = e.target.elements.commandInput.value;
     e.target.elements.commandInput.value = '';
-    console.log(command);
     socket.emit('directCommand', command);
 });
--- data/node/src/index.js
+++ data/node/src/index.js
@@ -23,19 +23,22 @@ rclnodejs.init()
         const node = rclnodejs.createNode('publisher_example_node');
 
         const rospublisher = node.createPublisher('std_msgs/msg/String', 'send_message');
-        
-
+        const subscription = node.createSubscription('std_msgs/msg/String', 'chatter', (msg) => {
+            console.log(chalk.blue.bold(`Received message:`), `${typeof msg}`, msg);
+            topic_message = msg;
+            io.emit('message', topic_message);
+            
+        });
+        console.log(subscription)
         io.on('connection', (socket) => {
             console.log("New connection!");
-            const subscription = node.createSubscription('std_msgs/msg/String', 'chatter', msg => {
-                console.log(chalk.blue.bold(`Received message:`), `${typeof msg}`, msg);
-                topic_message = msg;
-                socket.emit('message', topic_message)
-                socket.on('directCommand', (command)=> {
-                    console.log(command);
-                    socket.emit('message', command);
-                });
+            socket.emit('message', topic_message)
+            socket.on('directCommand', (command)=> {
+                console.log(command);
+                rospublisher.publish(command);
+                socket.emit('message', command);
             });
+            
         });
         
         
