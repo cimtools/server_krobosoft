e854a9c2c700d5321771d5f093bdb954565317fa
GiulianoWF <Giuliano.Floriani@edu.pucrs.br>
2020-04-04 22:40:03 -0300

    Added ros2 echo node in data/C++.

---
+++ data/C++/krobosoft_pkg/build/krobosoft_pkg/CMakeFiles/krobosoft_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/krobosoft_node.dir/src/krobosoft_node.cpp.o"
+  "krobosoft_node.pdb"
+  "krobosoft_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/krobosoft_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ data/C++/krobosoft_pkg/src/krobosoft_node.cpp
@@ -0,0 +1,41 @@
+#include <memory>
+#include <iostream>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using std::placeholders::_1;
+
+class MinimalEcho : public rclcpp::Node
+{
+  public:
+    MinimalEcho()
+    : Node("krobosoft_node"){
+        publisher_ = this->create_publisher<std_msgs::msg::String>(
+            "server_in", 
+            10
+        );
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "server_out",
+            10,
+            std::bind(&MinimalEcho::callback, this, _1)
+        );
+    }
+
+  private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+    void callback(const std_msgs::msg::String::SharedPtr msg) {
+        publisher_->publish(*msg);
+        std::cout << msg->data << std::endl;
+    }
+  };
+
+  int main(int argc, char * argv[])
+  {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalEcho>());
+    rclcpp::shutdown();
+    return 0;
+  }
