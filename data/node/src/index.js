e4fadc58c66327f811fb0094e5cf5e1e4a97a37e
GiulianoWF <Giuliano.Floriani@edu.pucrs.br>
2020-04-05 14:58:45 -0300

    Added full communication loop.

--- data/node/src/index.js
+++ data/node/src/index.js
@@ -15,36 +15,44 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 var client_list = []
 var node_list = []
+var ros2_publisher;
+var ros2_subscriber;
 
+// Called when ROS2 is first initiated. It setups ROS2 com.
 function add_node(node) {
-    const rospublisher = node.createPublisher('std_msgs/msg/String', 'send_message');
-    const subscription = node.createSubscription('std_msgs/msg/String', 'chatter', (msg) => {
+    node_list.push(node);
+    ros2_publisher = node.createPublisher('std_msgs/msg/String', 'server_out');
+    ros2_subscriber = node.createSubscription('std_msgs/msg/String', 'server_in', (msg) => {
         console.log(chalk.blue.bold(`Received message:`), `${typeof msg}`, msg);
-        // io.emit('message', msg);
+        // TODO: Insert business logic to forward messages.
+        if (client_list.length > 0){
+            client_list[0].emit('message', msg);
+        }
     });
 }
 
 function add_client(socket) {
     client_list.push(socket);
-    console.log("New connection! Id:");
-    console.log(socket.id);
-    console.log('Current number of connections: ', client_list.length);
+    console.log("New connection! Id:", socket.id);
+
+    // Receives message from client and send to ROS2 topic.
     socket.on('directCommand', (command)=> {
-        console.log(command);
-        // rospublisher.publish(command);
-        // socket.emit('message', command);
+        // TODO: Insert business logic to forward messages.
+        console.log(socket.id, ': ', command);
+        ros2_publisher.publish(command);
     });
 
+    // Set the disconnect callback to this client.
     socket.on('disconnect', ()=> {
         remove_client(socket.id);
     });
 }
 
+// Handles disconnect. Delete client from client list.
 function remove_client(id) {
     console.log('disconect', id);
     var indexOfSocket = client_list.findIndex(i => i.id === id);
     client_list.splice(indexOfSocket, 1);
-    console.log('Index disconected: ', indexOfSocket);
 }
 
 app.use(express.static(publicDirectoryPath));
@@ -54,14 +62,13 @@ rclnodejs.init()
         const node = rclnodejs.createNode('krobosoft_node');
         add_node(node);
         rclnodejs.spin(node);
-        return node;
+    })
+    .then(()=>{
+        server.listen(port, () => {
+            console.log(chalk.green(`Server is up on port ${port}!`));
+        });
+        io.on('connection', add_client); 
     });
-
-io.on('connection', add_client);  
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-server.listen(port, () => {
-    console.log(chalk.green(`Server is up on port ${port}!`));
-});
