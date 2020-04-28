d06a9c006d36ec1e6bcc95771c4829baae0a0794
GiulianoWF <Giuliano.Floriani@gmail.com>
2020-04-28 08:09:05 -0300

    Change line terminations in the ros2 container entrypoint.

--- python-ros2/entry.bash
+++ python-ros2/entry.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-cp /home/extern/krobosoft_stt_node.py /home/ws/src/krobosoft_stt/krobosoft_stt/krobosoft_stt_node.py 
-cd /home/ws/ 
+cp /home/extern/krobosoft_stt_node.py /home/ws/src/krobosoft_stt/krobosoft_stt/krobosoft_stt_node.py
+cd /home/ws/
 source /opt/ros/eloquent/setup.bash
 colcon build 
 source /home/ws/install/setup.bash
